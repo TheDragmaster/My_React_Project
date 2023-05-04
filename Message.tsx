619f4d0be7591c5874fd7c3b992069fda9b523ef
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-05-03 21:08:14 -0500

    Add files via upload
    Just the basic typescript used to import and render an element

---
+++ Message.tsx
@@ -0,0 +1,10 @@
+ // PascalCasing basically capitalize the first letter of each word 
+ function Message() {
+    // JSX
+    const name ='Adrian';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+ }
+
+ export default Message;
